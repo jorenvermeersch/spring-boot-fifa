7754c10aa5e28bfb78db5b2981e503123d829bbd
jorenvermeersch <joren.vermeersch@student.hogent.be>
2022-04-25 19:36:04 +0200

    Created Spring Boot Project

---
+++ Spring_Boot_Examenopdracht_Fifa/src/main/java/com/springBoot/fifa/SpringBootExamenopdrachtFifaApplication.java
@@ -0,0 +1,13 @@
+package com.springBoot.fifa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExamenopdrachtFifaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExamenopdrachtFifaApplication.class, args);
+	}
+
+}
---
+++ Spring_Boot_Examenopdracht_Fifa/src/test/java/com/springBoot/fifa/SpringBootExamenopdrachtFifaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBoot.fifa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExamenopdrachtFifaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

fb246c079924ec94040339c341212c4e1ebbfe96
jorenvermeersch <joren.vermeersch@student.hogent.be>
2022-04-26 16:08:32 +0200

    Added dependencies and application properties

--- Spring_Boot_Examenopdracht_Fifa/src/main/java/com/springBoot/fifa/SpringBootExamenopdrachtFifaApplication.java
+++ Spring_Boot_Examenopdracht_Fifa/src/main/java/com/springBoot/fifa/SpringBootExamenopdrachtFifaApplication.java
@@ -2,12 +2,20 @@ package com.springBoot.fifa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class SpringBootExamenopdrachtFifaApplication {
+public class SpringBootExamenopdrachtFifaApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootExamenopdrachtFifaApplication.class, args);
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/css/**").addResourceLocations("resources/css/");
+		registry.addResourceHandler("/scripts/**").addResourceLocations("resources/scripts/");
 	}
 
 }

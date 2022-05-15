c875c6c2e9b6f2554b2fc0aebb560207f1a407aa
jorenvermeersch <joren.vermeersch@student.hogent.be>
2022-05-15 02:13:08 +0200

    Added REST controller for match details

---
+++ Spring_Boot_Examenopdracht_Fifa/src/main/java/com/springBoot/fifa/FifaDetailsController.java
@@ -0,0 +1,22 @@
+package com.springBoot.fifa;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import service.SoccerService;
+
+@RestController
+@RequestMapping(value = "/fifaDetail")
+public class FifaDetailsController {
+	
+	@Autowired
+	private SoccerService soccerService;
+	
+	@GetMapping(value = "/{id}")
+	public String[] getMatchCountries(@PathVariable("id") String matchId) {
+		return soccerService.getMatch(matchId).getMatch().getCountries(); 
+	}
+}
